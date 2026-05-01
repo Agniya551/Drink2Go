36fd3f3efbdc8c8b1ed09fc0462df6a40cd40171
Agniya551 <10hact10i@mail.ru>
2026-05-01 13:01:37 +0400

    Добавляет стили для шрифтов

--- gulpfile.js
+++ gulpfile.js
@@ -107,7 +107,7 @@ export function optimizeRaster () {
     return { formats };
   }
 
-  return src(`${PATH_TO_RAW}images/**/*.{png,jpg,jpeg}`)
+  return src(`${PATH_TO_RAW}images/**/*.{png,jpg,jpeg}`, { encoding: false, nodir: true })
     .pipe(sharp(createOptionsFormat()))
     .pipe(dest(`${PATH_TO_SOURCE}images`));
 }
